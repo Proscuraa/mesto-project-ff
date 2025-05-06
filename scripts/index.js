abe7c2a2ba69ac41cd5e3054527879536d20d625
proscuraa <proscuraa@yandex.com>
2025-05-06 12:41:07 +0300

    first commit JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardsContainer = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(card, deleteCallback) {
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    cardImage.src = card.link;
+    cardTitle.textContent = card.name;
+    cardImage.alt = `фотография места: ${card.name}`;
 
-// @todo: Функция создания карточки
+    deleteButton.addEventListener("click", () => {
+        deleteCallback(cardElement);
+    });
+    return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach((cardElement) => {
+    const card = createCard(cardElement, deleteCard);
+    cardsContainer.append(card);
+});
